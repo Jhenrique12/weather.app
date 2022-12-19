6bb7d76cea05a864ea87cd4764ab94981f7ee9da
Jhenrique12 <jonhenrique007@gmail.com>
2022-12-19 13:35:51 -0300

    Testing API

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("JavaScript it's working!")
+const apikey = "3265874a2c77ae4a04bb96236a642d2f"
+
+const url = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`
+
+async function getWeatherByLocation(location) {
+  const resp = await fetch(url(location), {
+    origin: "cors"
+  })
+  
+  const respData = await resp.json()
+
+  console.log(respData)
+}
+
+getWeatherByLocation("London")
