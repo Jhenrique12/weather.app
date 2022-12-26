bbff5055eb31082326d42545afdb5cd81f2dcad7
Jhenrique12 <jonhenrique007@gmail.com>
2022-12-26 13:14:00 -0300

    Add: form submite event to search location and add Celsius value in the page

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const apikey = "3265874a2c77ae4a04bb96236a642d2f"
 
+const main = document.getElementById('main')
+const form = document.getElementById('form')
+const inputSearch  = document.getElementById('search')
+
 const url = (location) => `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apikey}`
 
 async function getWeatherByLocation(location) {
@@ -9,9 +13,33 @@ async function getWeatherByLocation(location) {
 
   const respData = await resp.json()
 
-  console.log(respData, KelvinToCelsius(respData.main.temp))
+  addWeather(respData)
 }
 
-getWeatherByLocation("Fortaleza")
+function addWeather(data) {
+  const temp = KelvinToCelsius(data.main.temp)
+
+  const weather = document.createElement('div')
+  weather.classList.add('weather')
+
+  weather.innerHTML = `
+    <small>Está</small> 
+    <h2> ${temp}°C </h2>
+    <p> em ${inputSearch.value} </p>
+  `
+
+  main.appendChild(weather)
+}
 
 const KelvinToCelsius = (K) => (K - 273.15).toFixed(2)
+
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault()
+
+  const location = inputSearch.value 
+
+  if(location) {
+    getWeatherByLocation(location)
+  }
+
+})
